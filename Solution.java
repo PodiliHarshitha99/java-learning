79508b027468d0b9e56a856b92e8bfd46268fc35
Harshitha P <57754440+PodiliHarshitha99@users.noreply.github.com>
2024-09-26 11:31:56 -0500

    ObjectTypeToPrimitiveType.java

--- Solution.java
+++ Solution.java
@@ -5,9 +5,8 @@ import java.util.ArrayList;
 //import java.util.Collections;
 //import java.util.List;
 
-public class Solution {
-	
-	
+public class ObjectTypeToPrimitiveType {
+		
 	  public static void main(String[] args) {
 		  
 		  ArrayList<Object> list = new ArrayList<Object>();
@@ -53,69 +52,3 @@ public class Solution {
 		  System.out.println("Boolean type array list: " +bool_list);
 	  }
 	}
-////ArrayList<Boolean> list = new ArrayList<>();
-//
-//ArrayList<Integer> list2 = new ArrayList<>();
-//list2.add();
-//
-////int extraCandies;
-//
-//for(int i : list2) {
-//i=Collections.max(list2);
-//System.out.println(i);
-
-
-
-
-
-
-
-	/*public List<Boolean> kidsWithCandies(int[] candies, int extraCandies){
-// Creating kidsWithCandies method that takes integer array as 1st parameter and another integer as a 2nd parameter. 
-//It returns a list of boolean values 
-	
-		ArrayList<Boolean> list = new ArrayList<>();
-		
-		
-		
-		return list;
-		
-	*/
-	
-		
-		
-		
-	/*	
-		
-		int max = 0;
-		for(int i : candies) {
-			if (i>max) {
-				max=i;
-			}
-		}
-		for(int i : candies) {
-			
-			list.add(i + extraCandies >= max);
-			
-					
-		}
-				
-		
-        return list;
-    }
-    */
-
-  
-    	
-    	
-        
-    	
-    	/*Solution solution = new Solution();
-        int[] candies = {2, 3, 5, 1, 3};
-        int extraCandies = 3;
-        List<Boolean> result = solution.kidsWithCandies(candies, extraCandies);
-        System.out.println(result);
-        */ 
-  
-
-
